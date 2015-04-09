d19951d569d64cec29c64dc6f4f75f76fa1ac18c
rkouere <rkouere@gmail.com>
2015-04-09 10:40:58 +0200

    Ajout du calcul des valeurs max et min à la class traitant un seul fichier.

--- pjiSpectogramAnalysis/src/com/datacrunshing/Main/AverageSingleSample.java
+++ pjiSpectogramAnalysis/src/com/datacrunshing/Main/AverageSingleSample.java
@@ -20,7 +20,10 @@ import java.nio.ByteBuffer;
 public class AverageSingleSample extends Average {
     // the number of measures present in the file
     private int numberOfOctet = 0;
-
+    private long maxValue;
+    private long minValue;
+    private boolean started = false;
+    
     public AverageSingleSample(String[] args) throws FileNotFoundException {
         if(args.length != 1)
             Tools.displayErrorAndExit("Le programe ne prend qu'un argument : le fichier a traiter");
@@ -32,20 +35,34 @@ public class AverageSingleSample extends Average {
     public void exportFile() throws IOException {
         byte[] buffer = new byte[Tools.dataSize];
         long result = 0;
-        int bytesRead;
-        
+        long tmp;
         /* Pour chaque 32 bits, on va lire le premier fichier et ajouter sa valeur dans result.
             On va ensuite faire de même pour les autres et calculer sa moyenne.
         */
-        while ((bytesRead = fileInputStream[0].read(buffer)) != -1)     
+        while (fileInputStream[0].read(buffer) != -1)     
         {
-
-            result += byteToLong(buffer);
-
+            tmp = byteToLong(buffer);
+            // si c'est notre premiere passe, on initialise les valeur max et min (sinon, elle seront initialisé à 0 par défault et les résultat que nous aurons ne seront pas formcément les bon)
+            if(started == false) {
+                this.maxValue = this.minValue = tmp;
+                started = true;
+            }
+            // on met à jours les valeurs max et min, si besoin
+            else {
+                if(tmp > this.maxValue)
+                    this.maxValue = tmp;
+                if(tmp < this.minValue)
+                    this.minValue = tmp;
+            }
+            // on ajoute la valeur à notre resultat final
+            result += tmp;
             this.numberOfOctet ++;
         }
         
-        System.out.println("La mesure  moyenne est de : " + result/this.numberOfOctet);
+        System.out.println("La mesure moyenne est de : " + result/this.numberOfOctet);
+        System.out.println("La mesure maximum est de : " + this.maxValue);
+        System.out.println("La mesure minimum est de : " + this.minValue);
+        
     }
     
 }
